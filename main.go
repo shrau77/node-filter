60e4dd55ef8cc56357e53d7b48716fe6bee21640
shrau77 <shray@duck.com>
2026-02-01 23:47:56 +0300

    Update main.go

--- main.go
+++ main.go
@@ -26,16 +26,16 @@ import (
 )
 
 const (
-	WorkerCount       = 80
+	WorkerCount       = 120
 	MinPortRange      = 20000
 	MaxPortRange      = 35000
 	XrayURL           = "https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip"
 	GeoIPURL          = "https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-Country.mmdb"
 	TestURL           = "https://www.youtube.com/generate_204"
-	SpeedTestURL      = "https://download.microsoft.com/download/2/0/E/20E90413-712F-438C-988E-FDAA79A8AC3D/dotnetfx35.exe"
+	SpeedTestURL      = "https://speed.cloudflare.com/__down?bytes=2097152" 
 	SpeedTestSize     = 2 * 1024 * 1024 // 2MB
-	TimeoutCheck      = 7 * time.Second
-	TimeoutSpeed      = 7 * time.Second
+	TimeoutCheck      = 15 * time.Second
+	TimeoutSpeed      = 20 * time.Second
 	UltraFastSpeed    = 6.0 // Mbps
 	FastSpeed         = 3.0 // Mbps
 )
